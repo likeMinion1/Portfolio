646e8e09b52c56c4062d26410b40e68386a5b000
likeMinion1 <joealexanderon@gmail.com>
2023-05-14 00:17:09 -0700

    first commit

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,9 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Noakhali Government College',
+        institution: 'University of Montreal',
         course: 'Bachelor Degree',
-        startYear: '2020',
-        endYear: 'Present'
-    },
-    {
-        id: 2,
-        institution: 'Noakhali Islamia Kamil Madrasah',
-        course: 'Higher Secondary',
-        startYear: '2018',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        institution: 'Baitus Saif Islamia Madrasah',
-        course: 'Secondary School',
-        startYear: '2008',
-        endYear: '2018'
-    },
+        startYear: '2013',
+        endYear: '2017'
+    }
 ]
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 
 export const headerData = {
-    name: 'ABU SAID',
+    name: 'Like Sunny',
     title: "Hello! I am",
-    desciption: "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
+    desciption: "Hello! I am Like Sunny, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. "+
+            "I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    image: 'https://i.ibb.co/n8Vhjg1/autralia-3.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
+    resumePdf: 'https://docs.google.com/document/d/1F-LjZ6HgSqvrkaTd430h57wHp_sB3JeNhZVipHP476g/edit?usp=sharing'
 }
